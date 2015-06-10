bd16c3b2bc02e810b46fa7b6f6a67500548925a6
Scott Donaldson <scott.p.donaldson@gmail.com>
2015-06-10 18:48:42 -0400

    kill contact form for Twitter follow button

--- digital-invisibles.php
+++ digital-invisibles.php
@@ -3,36 +3,6 @@
 Template Name: Digital Invisibles
 */
 
-// if submitting the form
-if ( isset($_POST['submit']) ) {
-
-	if ( isset($_POST['interested'])) {
-		die();
-	} else {
-
-		// collect $_POST variables
-		$submitter_name = $_POST['submitter-name'];
-		$submitter_email = $_POST['submitter-email'];
-		$invisible_name = $_POST['invisible-name'];
-		$information = isset($_POST['information']) ? $_POST['information'] : '';
-
-		// clean them up
-		$submitter_name = htmlspecialchars($submitter_name);
-		$submitter_email = htmlspecialchars($submitter_email);
-		$invisible_name = htmlspecialchars($invisible_name);
-		$information = htmlspecialchars($information);
-
-		$email_to = "arielleal@gmail.com, scott.p.donaldson@gmail.com";
-		$email_subject = "Digital Invisible: " . $invisible_name;
-		$email_body = $submitter_name . " (" . $submitter_email . ") submitted the following Digital Invisible:\n\n";
-		$email_body .= $invisible_name . "\n\n";
-		$email_body .= $information;
-
-		mail($email_to, $email_subject, $email_body);
-	}
-
-}
-
 get_header(); the_post(); ?>
 
 	<script src="<?= get_stylesheet_directory_uri(); ?>/underscore.js"></script>
@@ -56,40 +26,12 @@ get_header(); the_post(); ?>
 
 	            	<div class="the-content">
 
-						<?php if (!isset($_POST['submit'])) { ?>
-	            		<div class="row-fluid">
-
-		            		<h2 class="caps">Submit an Invisible</h2>
-
-							<form action="<?php the_permalink(); ?>#primary" id="digital-invisibles-submit" method="POST">
-
-								<div class="row-fluid">
-									<label for="submitter-name">Your name</label>
-									<input type="text" id="submitter-name" name="submitter-name" required>
-								</div>
-
-								<div class="row-fluid">
-									<label for="submitter-email">Your email</label>
-									<input type="email" id="submitter-email" name="submitter-email" required>
-								</div>
-
-								<div class="row-fluid">
-									<label for="invisible-name">Name of "invisible"</label>
-									<input type="text" id="invisible-name" name="invisible-name" required>
-								</div>
-
-								<div class="row-fluid">
-									<label for="information">More info</label>
-									<textarea name="information" id="information"></textarea>
-								</div>
-
-								<input type="checkbox" name="interested" style="display: none;">
-
-								<input type="submit" value="Submit" name="submit">
-							</form>
-
-		            	</div>
-						<?php } ?>
+						<p><strong>Follow Design for Equality on Twitter, and tell us who you think deserves to be represented on Wikipedia.<br><br>
+							<span style="display: block; text-align: center;">
+							<a href="https://twitter.com/design_equality" class="twitter-follow-button" data-show-count="false">Follow @design_equality</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+							</span>
+					</strong></p>
 
 						<?php
 
